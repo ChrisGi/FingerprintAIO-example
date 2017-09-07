5a45ddbd26adc6661fec354444707d6ff50a6a6e
Krzysztof Gradzki <krzysztof.gradzki@icompass.pl>
2017-09-07 11:56:22 +0200

    add scanning fingerprint in ios

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,33 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { PromiseObservable } from 'rxjs/observable/PromiseObservable';
+import { FingerprintAIO } from '@ionic-native/fingerprint-aio'
 
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
+  providers: [ FingerprintAIO ]
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public fingerprintAIO: FingerprintAIO) {
 
   }
 
+  public showScanFingerModal(): void {
+    console.log('Show modal');
+    PromiseObservable.create(
+      this.fingerprintAIO.show({
+        clientId: 'Fingerprint-Demo',
+        clientSecret: 'password', //Only necessary for Android
+        disableBackup:true,  //Only for Android(optional)
+        localizedFallbackTitle: 'Use Pin', //Only for iOS
+        localizedReason: 'Please authenticate' //Only for iOS
+      }))
+      .subscribe(
+        result => console.log('Success',
+        error => console.error('Error', error)
+      ));
+    
+  }
 }
